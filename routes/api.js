ecf37cfd4337e514a6d85ea5097e585e888ec340
Mike <Mike@Mikes-MacBook-Pro.local>
2016-02-24 18:36:31 -0500

    Update to routes.js

--- routes/api.js
+++ routes/api.js
@@ -10,90 +10,91 @@ router.get('/', function(req, res) {
   res.send('Welcome to the API zone');
 });
 
+// GET pages index
 router.get('/pages', function(request, response) {
 
-        return Page.find(function(err, pages) {
-            if (!err) {
-                return response.send(pages);
-            } else {
-                return response.send(500, err);
-            }
-        });
+    return Page.find(function(err, pages) {
+        if (!err) {
+            return response.send(pages);
+        } else {
+            return response.send(500, err);
+        }
+    });
+});
+
+// POST new page
+router.post('/pages/add', function(request, response) {
+    var page = new Page({
+        title: request.body.title,
+        url: request.body.url,
+        content: request.body.content,
+        menuIndex: request.body.menuIndex,
+        date: new Date(Date.now())
     });
 
-router.post('/pages/add', function(request, response) {
-        var page = new Page({
+    page.save(function(err) {
+        if (!err) {
+            return response.send(200, page);
+
+        } else {
+            return response.send(500,err);
+        }
+    });
+});
+
+// GET page via id
+router.get('/pages/admin-details/:id', function(request, response) {
+    var id = request.params.id;
+    Page.findOne({
+        _id: id
+    }, function(err, page) {
+        if (err)
+            return console.log(err);
+        return response.send(page);
+    });
+});
+
+// GET page via SEO-friendly URL
+router.get('/pages/details/:url', function(request, response) {
+    var url = request.params.url;
+    Page.findOne({
+        url: url
+    }, function(err, page) {
+        if (err)
+            return console.log(err);
+        return response.send(page);
+    });
+});
+
+// UPDATE page
+router.post('/pages/update', function(request, response) {
+    var id = request.body._id;
+    // Hardcoded document id for testing only
+    // var id = "56ce09443a8286f2078c1ece";
+
+    Page.update({
+        _id: id
+    }, {
+        $set: {
             title: request.body.title,
             url: request.body.url,
             content: request.body.content,
             menuIndex: request.body.menuIndex,
             date: new Date(Date.now())
-        });
+        }
+    }).exec();
+    response.send("Page updated");
+});
 
-        page.save(function(err) {
-            if (!err) {
-                return response.send(200, page);
-
-            } else {
-                return response.send(500,err);
-            }
-        });
+// DELETE page
+router.get('/pages/delete/:id', function(request, response) {
+    var id = request.params.id;
+    Page.remove({
+        _id: id
+    }, function(err) {
+        return console.log(err);
     });
-
-    router.get('/pages/admin-details/:id', function(request, response) {
-        var id = request.params.id;
-        Page.findOne({
-            _id: id
-        }, function(err, page) {
-            if (err)
-                return console.log(err);
-            return response.send(page);
-        });
-    });
-
-    router.get('/pages/details/:url', function(request, response) {
-        var url = request.params.url;
-        Page.findOne({
-            url: url
-        }, function(err, page) {
-            if (err)
-                return console.log(err);
-            return response.send(page);
-        });
-    });
-
-    router.get('/pages/admin-details/:id', function(request, response) {
-        var id = request.params.id;
-        Page.findOne({
-            _id: id
-        }, function(err, page) {
-            if (err)
-                return console.log(err);
-            return response.send(page);
-        });
-    });
-
-    router.get('/pages/details/:url', function(request, response) {
-        var url = request.params.url;
-        Page.findOne({
-            url: url
-        }, function(err, page) {
-            if (err)
-                return console.log(err);
-            return response.send(page);
-        });
-    });
-
-    router.get('/pages/delete/:id', function(request, response) {
-        var id = request.params.id;
-        Page.remove({
-            _id: id
-        }, function(err) {
-            return console.log(err);
-        });
-        return response.send('Page id- ' + id + ' has been deleted');
-    });
-
-
+    return response.send('Page id- ' + id + ' has been deleted');
+});
 
 module.exports = router;
