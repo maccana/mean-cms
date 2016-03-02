de2a24029b48c28f094d7b8263ddf2e2ad588697
Mike <mikeymccann@gmail.com>
2016-03-02 16:25:55 -0500

    Update to fix issues loading angular index page and partials

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 // Set default route to index of client-side Angular app
-router.get('*', function(request, response) {
-    response.sendfile('./public/app/index.html');
+router.get("/", function(req, res)
+{
+    //res.sendFile("/public/app/index.html");
+    res.sendFile(path.join(__dirname, '../public/app', 'index.html'));
 });
 
-/* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
+router.get('/views/:name', function (req, res) {
+  var name = req.params.name;
+  console.log("Patial loading:", name);
+
+  // NOTE: _dirname is ends '..../mean-cms/routes'
+  // '../public/app/views' goes back up a directory to find angular templates
+  res.render(path.join(__dirname, '../public/app/views/' + name));
+
+});
 
 module.exports = router;
