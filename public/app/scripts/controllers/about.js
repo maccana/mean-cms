2d1ec2c00a7fa3b77ef964c4ec5c22c881536f21
Mike <mikeymccann@gmail.com>
2016-03-02 16:36:56 -0500

    Add Angular app

---
+++ public/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name newyoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the newyoApp
+ */
+angular.module('newyoApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ public/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name newyoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the newyoApp
+ */
+angular.module('newyoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
